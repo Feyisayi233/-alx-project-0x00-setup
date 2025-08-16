7e64188dfb37e12b4b0165cc8cca4bbb10f19797
Feyisayo <feyisayo233@gmail.com>
2025-08-16 13:59:27 +0100

    task 6

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, className }) => {
+  return <button className={className}>{title}</button>;
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <div className="flex gap-4 mb-8">
+        <Button
+          title="Small Rounded"
+          className="px-3 py-1 text-sm rounded-sm bg-blue-500 text-white"
+        />
+        <Button
+          title="Medium Rounded"
+          className="px-4 py-2 text-base rounded-md bg-green-500 text-white"
+        />
+        <Button
+          title="Large Rounded"
+          className="px-6 py-3 text-lg rounded-full bg-red-500 text-white"
+        />
+      </div>
       <Card />
       <Card />
       <Card />
